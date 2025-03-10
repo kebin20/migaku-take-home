f6a070be39acde6750ac3ffe570a7d2fe187bb76
kebin20 <ktanzyl@hotmail.com>
2025-03-10 15:32:56 +0900

    chore: installed auto import package

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      dts: true,
+    }),
+    AutoImport({
+      imports: ['vue'],
+      dts: true,
+    }),
     vueJsx(),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
