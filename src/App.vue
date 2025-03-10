7f5026451e415633d7e934e5d40e2acc9ceaaf78
kebin20 <ktanzyl@hotmail.com>
2025-03-10 14:19:46 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <button>Monkey</button>
+    <ProgressBar />
+  </header>
+  <main>
+    <MigakuHeroSection />
+    <TodayCardsList />
+    <CompletedCardsList />
+  </main>
+  <footer>
+    <BottomNav />
+  </footer>
+</template>
+
+<style scoped></style>
