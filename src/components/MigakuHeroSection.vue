8d6876d88000b4c5a833b9116b997010e0f74a9e
kebin20 <ktanzyl@hotmail.com>
2025-03-11 12:39:52 +0900

    chore: completed hero section

--- src/components/MigakuHeroSection.vue
+++ src/components/MigakuHeroSection.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="hero-section">
-    <img src="@\assets\icons\planets-a.png" alt="Image 1" class="image purple-planet" />
-    <img src="@\assets\icons\planets-b.png" alt="Image 2" class="image comet" />
-    <img src="@\assets\icons\migaku-heart.png" alt="Image 3" class="image migaku-heart" />
-    <img src="@\assets\icons\planets-d.png" alt="Image 5" class="image planets" />
-    <img src="@\assets\icons\planets-c.png" alt="Image 4" class="image earth" />
+    <img src="@\assets\icons\hero\planets-a.png" alt="Image 1" class="image purple-planet" />
+    <img src="@\assets\icons\hero\planets-b.png" alt="Image 2" class="image comet" />
+    <img src="@\assets\icons\hero\migaku-heart.png" alt="Image 3" class="image migaku-heart" />
+    <img src="@\assets\icons\hero\planets-d.png" alt="Image 5" class="image planets" />
+    <img src="@\assets\icons\hero\planets-c.png" alt="Image 4" class="image earth" />
   </div>
 </template>
 
@@ -14,7 +14,8 @@
 .hero-section {
   position: relative;
   width: 100%;
-  height: 100px;
+  height: 150px;
+  z-index: -1;
 }
 
 .image {
@@ -22,15 +23,15 @@
 }
 
 .purple-planet {
-  width: 46%;
-  top: -63%;
+  width: 47%;
+  top: -43%;
   left: 3%;
   z-index: -1;
 }
 
 .comet {
   width: 14%;
-  top: -64%;
+  top: -43%;
   left: 66%;
 }
 
@@ -42,14 +43,14 @@
 }
 
 .earth {
-  width: 31%;
-  top: 96%;
-  left: 4%;
+  width: 27%;
+  top: 46%;
+  left: 2%;
 }
 
 .planets {
-  top: 29%;
-  left: 53%;
-  width: 43%;
+  top: 23%;
+  left: 60%;
+  width: 33%;
 }
 </style>
